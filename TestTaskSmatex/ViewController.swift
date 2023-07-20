057002fb4263bbc8ca18960670f14f67987eb250
Shishkanov Maxim <shishkanovma@bk.ru>
2023-07-20 16:13:37 +0400

    Initial Commit

---
+++ TestTaskSmatex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTaskSmatex
+//
+//  Created by Максим Шишканов on 20.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
